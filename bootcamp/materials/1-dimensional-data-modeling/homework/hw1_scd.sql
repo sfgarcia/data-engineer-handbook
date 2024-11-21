1b4fd84a0b42c5f281ad190f7fea368ba645c41d
sfgarcia <sfgarcia@uc.cl>
2024-11-20 21:28:31 -0300

    Finish hw1

--- bootcamp/materials/1-dimensional-data-modeling/homework/hw1_scd.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/hw1_scd.sql
@@ -1,132 +1,83 @@
-create type film_struct as (
-    film text,
-    votes integer,
-    rating real,
-    filmid text
-)
+-- select year,count(*)
+-- from actor_films
+-- group by 1
+-- order by 1
 
-create type quality_class as enum ('star', 'good', 'average', 'bad')
 
-create table actors (
-    actorid text,
-    actor text,
-    films film_struct[],
-    quality quality_class,
-    is_active boolean,
-    current_year integer,
-    primary key (actorid, current_year)
-)
+-- select *
+-- from actors
+-- limit 10
 
-insert into actors
+-- create table actors_history_scd (
+--     actorid text,
+--     actor text,
+--     quality_class quality_class,
+--     is_active boolean,
+--     start_year integer,
+--     end_year integer,
+--     --current_year integer,
+--     primary key (actorid, start_year)
+-- )
 
-with yesterday as (
-    select *
-    from actors
-    where current_year = 1971
-),
-today as (
-    select actorid,
-        actor,
-        year,
-        array_agg(row(film, votes, rating, filmid)::film_struct) as films
-    from actor_films
-    where year = 1972
-    group by 1, 2, 3
-),
-rating as (
-    select actorid,
-        case
-            when avg(rating) > 8 then 'star'
-            when avg(rating) > 7 then 'good'
-            when avg(rating) > 6 then 'average'
-            else 'bad'
-        end::quality_class as quality_class
-    from actor_films
-    where year = 1972
-    group by 1
-)
+-- full history scd query
+-- insert into actors_history_scd
+-- with previous as (
+--     select
+--         actorid,
+--         actor,
+--         quality as quality_class,
+--         lag(quality, 1) over (partition by actorid order by current_year) as prev_quality_class,
+--         is_active,
+--         lag(is_active, 1) over (partition by actorid order by current_year) as prev_is_active,
+--         current_year
+--     from actors
+--     where current_year < 2000
+-- ),
+-- detect_changes as (
+--     select
+--         actorid,
+--         actor,
+--         quality_class,
+--         is_active,
+--         current_year,
+--         case
+--             when prev_quality_class != quality_class then 1
+--             when prev_is_active != is_active then 1
+--             else 0
+--         end as scd_change
+--     from previous
+-- ),
+-- scd_groups as (
+--     select
+--         actorid,
+--         actor,
+--         quality_class,
+--         is_active,
+--         current_year,
+--         sum(scd_change) over (partition by actorid order by current_year) as scd_group
+--     from detect_changes
+-- )
 
-select
-    COALESCE(y.actorid, t.actorid) as actorid,
-    COALESCE(y.actor, t.actor) as actor,
-    case
-        when y.films is null then t.films
-        else y.films || t.films
-    end as films,
-    r.quality_class,
-    case
-        when t.actorid is null then false
-        else true
-    end as is_active,
-    coalesce(t.year, y.current_year + 1) as current_year
-from yesterday y
-full outer join today t on y.actorid = t.actorid
-left join rating r on t.actorid = r.actorid
+-- select
+--     actorid,
+--     actor,
+--     quality_class,
+--     is_active,
+--     min(current_year) as start_year,
+--     max(current_year) as end_year
+-- from scd_groups
+-- group by 1, 2, 3, 4
+--order by 1, 2, 3, 4
+--limit 10
 
-create table actors_history_scd (
-    actorid text,
-    actor text,
-    quality_class quality_class,
-    is_active boolean,
-    start_year integer,
-    end_year integer,
-    primary key (actorid, start_year)
-)
+-- create type scd_type as (
+--     quality_class quality_class,
+--     is_active boolean,
+--     start_year integer,
+--     end_year integer
+-- )
 
-with previous as (
-    select
-        actorid,
-        actor,
-        quality as quality_class,
-        lag(quality, 1) over (partition by actorid order by current_year) as prev_quality_class,
-        is_active,
-        lag(is_active, 1) over (partition by actorid order by current_year) as prev_is_active,
-        current_year
-    from actors
-    where current_year < 2000
-),
-detect_changes as (
-    select
-        actorid,
-        actor,
-        quality_class,
-        is_active,
-        current_year,
-        case
-            when prev_quality_class != quality_class then 1
-            when prev_is_active != is_active then 1
-            else 0
-        end as scd_change
-    from previous
-),
-scd_groups as (
-    select
-        actorid,
-        actor,
-        quality_class,
-        is_active,
-        current_year,
-        sum(scd_change) over (partition by actorid order by current_year) as scd_group
-    from detect_changes
-)
-
-select
-    actorid,
-    actor,
-    quality_class,
-    is_active,
-    min(current_year) as start_year,
-    max(current_year) as end_year
-from scd_groups
-group by 1, 2, 3, 4
-
-create type scd_type as (
-    quality_class quality_class,
-    is_active boolean,
-    start_year integer,
-    end_year integer
-)
-
+---- incremental history scd query
 insert into actors_history_scd
 with historical_scd as (
     select *
@@ -362,3 +313,5 @@ union_records as (
 
 select *
 from union_records
+limit 10
+
