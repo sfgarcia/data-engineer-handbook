3961749faaa7efd9084ed361e495295b24f4f437
sfgarcia <sfgarcia@uc.cl>
2024-11-30 12:01:05 -0300

    Save queries

--- bootcamp/materials/2-fact-data-modeling/homework/submission/dedup_game_details.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/submission/dedup_game_details.sql
@@ -1,2 +1,26 @@
+-- select *
+-- from game_details
+-- where game_id = 22000071
+--     and team_id = 1610612742
+--     and player_id = 1630179
+
+-- select
+--     game_id,
+--     team_id,
+--     player_id
+-- from game_details
+-- group by 1, 2, 3
+-- having count(*) > 1
+-- limit 10
+
+with deduped_game_details as (
+    select
+        *,
+        row_number() over (partition by game_id, team_id, player_id) as row_num
+    from game_details
+)
+
 select *
-from game_details
+from deduped_game_details
+where row_num = 1
+limit 10
