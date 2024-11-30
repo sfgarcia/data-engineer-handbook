520cb7581d695603fa5427b156976f49a24c9cc2
sfgarcia <sfgarcia@uc.cl>
2024-11-30 12:37:16 -0300

    Save query

--- bootcamp/materials/2-fact-data-modeling/homework/submission/cum_devices_datelist.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/submission/cum_devices_datelist.sql
@@ -1,67 +1,38 @@
--- INSERT INTO user_devices_cumulated
--- WITH activity AS (
---     SELECT
---         events.user_id,
---         devices.browser_type,
---         array_agg(
---             CASE
---                 WHEN events.event_time IS NOT NULL THEN events.event_time::date
---                 ELSE NULL
---             END
---         ) AS device_activity_datelist
---     FROM events
---     JOIN devices ON events.device_id = devices.device_id
---     WHERE user_id IS NOT NULL
---     GROUP BY 1, 2
--- )
--- SELECT
---     user_id,
---     browser_type,
---     device_activity_datelist
--- FROM activity
-
+-- TRUNCATE TABLE user_devices_cumulated;
+INSERT INTO user_devices_cumulated
 WITH yesterday AS (
     SELECT
+        curr_date,
         user_id,
         browser_type,
         device_activity_datelist
     FROM user_devices_cumulated
-    WHERE curr_date = date('2022-12-31')
+    WHERE curr_date = DATE('2023-01-02')
 ),
 today AS (
     SELECT DISTINCT
-        event_time::date AS curr_date,
+        event_time::DATE AS curr_date,
         events.user_id,
         devices.browser_type
     FROM events
     JOIN devices ON events.device_id = devices.device_id
     WHERE events.user_id IS NOT NULL
-        AND event_time::date = date('2023-01-01')
+        AND event_time::DATE = DATE('2023-01-03')
 )
 
 SELECT
     COALESCE(yesterday.user_id, today.user_id) AS user_id,
     COALESCE(yesterday.browser_type, today.browser_type) AS browser_type,
-    ARRAY[today.curr_date] || yesterday.device_activity_datelist AS device_activity_datelist,
-    today.curr_date
+    CASE WHEN yesterday.device_activity_datelist IS NULL THEN ARRAY[today.curr_date]
+        WHEN today.curr_date IS NULL THEN yesterday.device_activity_datelist
+        ELSE ARRAY[today.curr_date] || yesterday.device_activity_datelist END AS device_activity_datelist,
+    COALESCE(today.curr_date, yesterday.curr_date + interval '1' day) AS curr_date
 FROM yesterday
 FULL OUTER JOIN today
     ON yesterday.user_id = today.user_id
     AND yesterday.browser_type = today.browser_type;
 
 -- select *
--- from events
--- where user_id = 2780254311411550000
-
-WITH date_events AS (
-    SELECT DISTINCT
-        event_time::date AS event_date,
-        user_id,
-        device_id
-    FROM events
-    WHERE event_time::date = date('2023-01-01')
-)
-
-select *
-from date_events
-where user_id = 70132547320211180
+-- from user_devices_cumulated
+-- where cardinality(device_activity_datelist) > 1
+-- limit 10
---
+++ bootcamp/materials/2-fact-data-modeling/homework/submission/full_devices_datelist.sql
@@ -0,0 +1,21 @@
+INSERT INTO user_devices_cumulated
+WITH activity AS (
+    SELECT
+        events.user_id,
+        devices.browser_type,
+        array_agg(
+            CASE
+                WHEN events.event_time IS NOT NULL THEN events.event_time::date
+                ELSE NULL
+            END
+        ) AS device_activity_datelist
+    FROM events
+    JOIN devices ON events.device_id = devices.device_id
+    WHERE user_id IS NOT NULL
+    GROUP BY 1, 2
+)
+SELECT
+    user_id,
+    browser_type,
+    device_activity_datelist
+FROM activity
